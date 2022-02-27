49c43d4214b4ac8ef284ccfdec2b195ebe878476
Zillion <dtgaby17@gmail.com>
2022-02-27 20:20:40 +0200

    Updated to my Project

---
+++ main.bat
@@ -0,0 +1,47 @@
+:MainName
+cls
+@echo off
+color 30
+python --version >NUL 2>&1
+if errorlevel 1 goto CheckPython
+type logo
+title Skylanders Key Calculator
+set /p name="~~> Write the name you want for your .keys file: "
+if [%name%]==[] goto CheckName
+cls
+
+:MainUID
+cls
+type logo
+set /p uid="~~> Write your tag's UID: "
+if [%uid%]==[] goto CheckUID
+if "%uid:~8,1%"=="" (goto CheckUID)
+goto MainGetKeys
+
+:MainGetKeys
+echo.
+python sklykeys.py -u %uid%
+python sklykeys.py -u %uid% > %name%.%uid%.keys >nul 2>&1
+echo.
+pause
+exit
+
+:CheckName
+echo.
+echo ERROR - The name can't be empty.
+pause
+goto MainName
+
+:CheckUID
+echo.
+echo ERROR - Your UID can't be less than 8 Characters
+pause
+goto MainUID
+
+:CheckPython
+color 04
+echo ERROR - Python not installed.
+start https://www.python.org/
+pause
+exit
+
